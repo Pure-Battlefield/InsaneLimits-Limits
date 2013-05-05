7f735b36849799bec977eeb90a160796c20c824e
lebeauc <lebeauc@gmail.com>
2013-05-05 19:11:43 -0300

    updated to match content config sheet

--- Defaults/AboutInfoRequest.cs
+++ Defaults/AboutInfoRequest.cs
@@ -3,7 +3,7 @@ if( Regex.Match(player.LastChat, @"^\s*[!/](aboutpure)$", RegexOptions.IgnoreCas
 	string about_1 = "We use all maps (when full), default settings (incl.";
 	string about_2 = "vehicle spawns), modest tickets, a squad-preserving";
 	string about_3 = "team balancer, squad VOIP (TS3Sync) & many";
-	string about_4 = "admins. Open community: purebattlefield.reddit.com.";
+	string about_4 = "admins. Open community: purebattlefield.org.";
 
 	List<string> About = new List<string>();
 	About.Add(about_1);
