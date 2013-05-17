6fa9df690ef14d0155c3328772759eae8743c8c1
lebeauc <lebeauc@gmail.com>
2013-05-17 13:35:17 -0300

    Updated for Map Poll

--- RushServer/NewsOnSpawn.cs
+++ RushServer/NewsOnSpawn.cs
@@ -11,10 +11,10 @@ if(        !(
 
 /* NEWS SECTION */
   	
-string news_1 = "SERVER NEWS: May 10th"; 
-string news_2 = "If you're new to Pure Battlefield, try joining your"; 
-string news_3 = "squad on TeamSpeak!  Type !tsinfo for details.";
-string news_4 = "PURE Conquest server: server1.purebattlefield.org";
+string news_1 = "SERVER NEWS: May 17th"; 
+string news_2 = "Our new Full Rush Rotation Poll is Live!"; 
+string news_3 = "To take the poll, visit poll.purebattlefield.org;
+string news_4 = "This poll will help determin the map rotation";
 
 List<string> News = new List<string>(); 
 News.Add(news_1);
