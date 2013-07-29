0ef52adfd391486981181ab06cc23847c5bf2823
ratdart <ratdart@gmail.com>
2013-07-29 00:36:12 -0400

    Changed NewsOnSpawn

--- MainServer/NewsOnSpawn.cs
+++ MainServer/NewsOnSpawn.cs
@@ -11,10 +11,10 @@ if(        !(
 
 /* NEWS SECTION */
 
-string news_1 = "SERVER NEWS: July 8th"; 
-string news_2 = "A celebration of PURE's first anniversary!"; 
-string news_3 = "Saturday 7/27: 11-4 PDT / 2-7 EDT";
-string news_4 = "";
+string news_1 = "SERVER NEWS: July 29th"; 
+string news_2 = "Don't forget to join our TeamSpeak Server"; 
+string news_3 = "Server: teamspeak.purebattlefield.org";
+string news_4 = "Type in \"!tsinfo\" for more information.";
 
 List<string> News = new List<string>(); 
 News.Add(news_1);
