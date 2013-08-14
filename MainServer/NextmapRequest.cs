6c606cda58e289df24d48583c6d84be58725d773
Draegerr <tjy692@gmail.com>
2013-08-14 19:51:29 -0400

    Update NextmapRequest.cs
    Changed for compiling errors.

--- MainServer/NextmapRequest.cs
+++ MainServer/NextmapRequest.cs
@@ -1,13 +1,13 @@
 //Content in the ANNOUNCEMENTS section NEEDS to be customized for each server and must match between !nextmap limit and Announce Next Map and Mode limit (starting at line 44).
-
+ 
 // -----------------------------------------------------------------------
 //
 // DEFINITIONS AND TRIGGER CRITIERA FOR GAMETYPE SWITCHING ANNOUNCEMENTS
 //
 // -----------------------------------------------------------------------
-
+ 
 int numAnnouncements = 5;
-
+ 
 // This is not a struct because InsaneLimits was throwing all kinds of fucked-up
 // compile errors when I tried to make it a struct and I eventually gave up.
 List<string>[] announcementGameTypes = new List<string>[numAnnouncements];
@@ -17,32 +17,32 @@ bool[] announcementServerNameMatchRequired = new bool[numAnnouncements];
 string[] announcementServerName1 = new string[numAnnouncements];
 string[] announcementServerName2 = new string[numAnnouncements];
 List <string>[] announcementText = new List<string>[numAnnouncements];
-
+ 
 for( int i=0; i < numAnnouncements; i++ )
 {
   announcementGameTypes[i] = new List<string>();
         announcementText[i] = new List<string>();
 }
-
+ 
 // Each announcement will get triggered if & only if:
 // - gametype matches
 // - servername matches (these must be listed first in the array)
-// - population is within range 
-
+// - population is within range
+ 
 // Message replacement strings:
 // %m: next map name
 // %s: " (teams switch sides)" if next round is same map, "" otherwise
-
+ 
 // -----------------------------------------------------------------------
 //
 // ANNOUNCEMENTS
 //
 // -----------------------------------------------------------------------
-
+ 
 /*
 Below is an example of 4 different announcements. Please use this example to build the announcements for each server.
 These examples are from the Main server
-
+ 
 // damavand rush
 announcementGameTypes[0].Add( "RushLarge0" ); //Game types can be found in "BF3 friendly game modes" section
 announcementMinPlayers[0] = 1;
@@ -54,7 +54,7 @@ announcementText[0].Add( "Next map is based on players at start of next round:" 
 announcementText[0].Add( "> 20: Random map from \"Popular Rush\" rotation" );
 announcementText[0].Add( "> 32: Random map from \"Popular Conquest\" rotation" );
 announcementText[0].Add( "Damavand Peak repeats until then." );
-
+ 
 // popular rush
 announcementGameTypes[1].Add( "RushLarge0" );
 announcementMinPlayers[1] = 1;
@@ -66,7 +66,7 @@ announcementText[1].Add( "Next map is based on players at start of next round:" 
 announcementText[1].Add( "> 40: Random map from \"Popular Conquest\" rotation" );
 announcementText[1].Add( "< 10: Damavand Peak" );
 announcementText[1].Add( "Otherwise: %m%s" );
-
+ 
 // popular conquest
 // this must come before the other conquest because it has a server name match.
 announcementGameTypes[2].Add( "ConquestLarge0" );
@@ -80,7 +80,7 @@ announcementText[2].Add( "Next map is based on players at start of next round:" 
 announcementText[2].Add( "> 52: Random map from \"Full Conquest\" rotation" );
 announcementText[2].Add( "< 32: Random map from \"Popular Rush\" rotation" );
 announcementText[2].Add( "Otherwise: %m" );
-
+ 
 // full conquest (lower population)
 announcementGameTypes[3].Add( "ConquestLarge0" );
 announcementGameTypes[3].Add( "ConquestAssaultLarge0" );
@@ -92,7 +92,7 @@ announcementServerName2[3] = "";
 announcementText[3].Add( "Next map is based on players at start of next round:" );
 announcementText[3].Add( "< 32: Random map from \"Popular Rush\" rotation" );
 announcementText[3].Add( "Otherwise: %m" );
-
+ 
 // full conquest (higher population)
 announcementGameTypes[4].Add( "ConquestLarge0" );
 announcementGameTypes[4].Add( "ConquestAssaultLarge0" );
@@ -103,7 +103,7 @@ announcementServerName1[4] = "";
 announcementServerName2[4] = "";
 announcementText[4].Add( "Next map: %m" );
 */
-
+ 
 // damavand rush
 announcementGameTypes[0].Add( "RushLarge0" );
 announcementMinPlayers[0] = 1;
@@ -111,11 +111,10 @@ announcementMaxPlayers[0] = 64;
 announcementServerNameMatchRequired[0] = true;
 announcementServerName1[0] = "PURE BATTLEFIELD 1: Damavand Rush madness!";
 announcementServerName2[0] = "PURE BATTLEFIELD: Defaults | $25 TS giveaway Sat";
-announcementText[0].Add( "Next map is based on players at start of next round:" );
-announcementText[0].Add( "> 20: Random map from \"Popular Rush\" rotation" );
-announcementText[0].Add( "> 32: Random map from \"Popular Conquest\" rotation" );
-announcementText[0].Add( "Damavand Peak repeats until then." );
-
+announcementText[0].Add( "Next map: %m %s" );
+announcementText[0].Add( "(It may be different if the server population changes" );
+announcementText[0].Add( "enough to activate a different map rotation.)" );
+ 
 // popular rush
 announcementGameTypes[1].Add( "RushLarge0" );
 announcementMinPlayers[1] = 1;
@@ -123,11 +122,10 @@ announcementMaxPlayers[1] = 64;
 announcementServerNameMatchRequired[1] = false;
 announcementServerName1[1] = "";
 announcementServerName2[1] = "";
-announcementText[1].Add( "Next map is based on players at start of next round:" );
-announcementText[1].Add( "> 40: Random map from \"Popular Conquest\" rotation" );
-announcementText[1].Add( "< 10: Damavand Peak" );
-announcementText[1].Add( "Otherwise: %m%s" );
-
+announcementText[1].Add( "Next map: %m %s" );
+announcementText[1].Add( "(It may be different if the server population changes" );
+announcementText[1].Add( "enough to activate a different map rotation.)" );
+ 
 // popular conquest
 // this must come before the other conquest because it has a server name match.
 announcementGameTypes[2].Add( "ConquestLarge0" );
@@ -135,13 +133,12 @@ announcementGameTypes[2].Add( "ConquestAssaultLarge0" );
 announcementMinPlayers[2] = 1;
 announcementMaxPlayers[2] = 64;
 announcementServerNameMatchRequired[2] = true;
-announcementServerName1[2] = "PURE BATTLEFIELD 1: Rush/Conquest | Friendly | Teamplay";
+announcementServerName1[2] = "PURE BATTLEFIELD 1: Rush/Conquest | Friendly | Teamspeak";
 announcementServerName2[2] = "PURE BATTLEFIELD: Defaults | $25 TS giveaway Sat!";
-announcementText[2].Add( "Next map is based on players at start of next round:" );
-announcementText[2].Add( "> 52: Random map from \"Full Conquest\" rotation" );
-announcementText[2].Add( "< 32: Random map from \"Popular Rush\" rotation" );
-announcementText[2].Add( "Otherwise: %m" );
-
+announcementText[2].Add( "Next map: %m %s" );
+announcementText[2].Add( "(It may be different if the server population changes" );
+announcementText[2].Add( "enough to activate a different map rotation.)" );
+ 
 // full conquest (lower population)
 announcementGameTypes[3].Add( "ConquestLarge0" );
 announcementGameTypes[3].Add( "ConquestAssaultLarge0" );
@@ -150,10 +147,10 @@ announcementMaxPlayers[3] = 45;
 announcementServerNameMatchRequired[3] = false;
 announcementServerName1[3] = "";
 announcementServerName2[3] = "";
-announcementText[3].Add( "Next map is based on players at start of next round:" );
-announcementText[3].Add( "< 32: Random map from \"Popular Rush\" rotation" );
-announcementText[3].Add( "Otherwise: %m" );
-
+announcementText[3].Add( "Next map: %m %s" );
+announcementText[3].Add( "(It may be different if the server population changes" );
+announcementText[3].Add( "enough to activate a different map rotation.)" );
+ 
 // full conquest (higher population)
 announcementGameTypes[4].Add( "ConquestLarge0" );
 announcementGameTypes[4].Add( "ConquestAssaultLarge0" );
@@ -162,21 +159,21 @@ announcementMaxPlayers[4] = 64;
 announcementServerNameMatchRequired[4] = false;
 announcementServerName1[4] = "";
 announcementServerName2[4] = "";
-announcementText[4].Add( "Next map: %m" );
-
+announcementText[4].Add( "Next map: %m %s" );
+ 
 // -----------------------------------------------------------------------
 //
 // END ANNOUNCEMENTS
 //
 // -----------------------------------------------------------------------
-
-
+ 
+ 
 // -----------------------------------------------------------------------
 //
 // HUMAN-READABLE MAP AND GAMETYPE NAMES
 //
 // -----------------------------------------------------------------------
-
+ 
 /* BF3 friendly map names */
 Dictionary<string, string> Maps = new Dictionary<string, string>();
 Maps.Add("MP_001", "Grand Bazaar");
@@ -213,7 +210,7 @@ Maps.Add("XP5_001", "Operation Riverside");
 Maps.Add("XP5_002", "Nebandan Flats");
 Maps.Add("XP5_003", "Kiasar Railroad");
 Maps.Add("XP5_004", "Sabalan Pipeline");
-
+ 
 /* BF3 friendly game modes */
 Dictionary<string, string> Modes = new Dictionary<string, string>();    
 Modes.Add("ConquestLarge0", "Conquest Large");
@@ -237,79 +234,79 @@ Modes.Add("Scavenger0","Scavenger");
 // End Game modes
 Modes.Add("AirSuperiority0","Air Superiority");
 Modes.Add("CaptureTheFlag0","CaptureTheFlag");
-
-if( !Maps.ContainsKey(server.MapFileName) || 
-	!Maps.ContainsKey(server.NextMapFileName) || 
-	!Modes.ContainsKey(server.NextGamemode) )
+ 
+if( !Maps.ContainsKey(server.MapFileName) ||
+        !Maps.ContainsKey(server.NextMapFileName) ||
+        !Modes.ContainsKey(server.NextGamemode) )
 {
-	plugin.ConsoleWrite(plugin.R("[One of the NextMapInfo limits] ERROR: Map name or gametype name not found in dictionary"));
-	return false;
+        plugin.ConsoleWrite(plugin.R("[One of the NextMapInfo limits] ERROR: Map name or gametype name not found in dictionary"));
+        return false;
 }
 // -----------------------------------------------------------------------
 //
 // END MAP AND GAMETYPE NAMES
 //
 // -----------------------------------------------------------------------
-
+ 
 // -----------------------------------------------------------------------
 //
 // MAIN WORK BEGINS HERE
 //
 // -----------------------------------------------------------------------
-        
+       
 int? gameTypeSwitchingAnnouncementIndex = null;
 bool announcementFound = false;
 string replacedMsg;
-
+ 
 plugin.ConsoleWrite(plugin.R("%p_n%: !nextmap"));
-
+ 
 // Figure out which predefined gametype switching announcements applies right now.
 // requires gametype match, population within the defined range, and maybe a server name match
 for( int i=0; i < numAnnouncements; i++ )
 {
-	foreach( string gameType in announcementGameTypes[i] )
-	{
-		if( (server.Gamemode == gameType) &&
-			(server.PlayerCount >= announcementMinPlayers[i]) &&
-			(server.PlayerCount <= announcementMaxPlayers[i]) &&
-			(
-				!announcementServerNameMatchRequired[i] ||
-				(announcementServerName1[i] == server.Name) ||
-				(announcementServerName2[i] == server.Name) 
-			) &&
-			!announcementFound )
-		{
-			// match.  We will use this announcement.
-			gameTypeSwitchingAnnouncementIndex = i;
-			// stop searching -- prevents us from matching other announcements
-			// (e.g. we have two conquest announcements, don't want to require a server name
-			// match for both if not needed)
-			announcementFound = true;
-		}
-	}
+        foreach( string gameType in announcementGameTypes[i] )
+        {
+                if( (server.Gamemode == gameType) &&
+                        (server.PlayerCount >= announcementMinPlayers[i]) &&
+                        (server.PlayerCount <= announcementMaxPlayers[i]) &&
+                        (
+                                !announcementServerNameMatchRequired[i] ||
+                                (announcementServerName1[i] == server.Name) ||
+                                (announcementServerName2[i] == server.Name)
+                        ) &&
+                        !announcementFound )
+                {
+                        // match.  We will use this announcement.
+                        gameTypeSwitchingAnnouncementIndex = i;
+                        // stop searching -- prevents us from matching other announcements
+                        // (e.g. we have two conquest announcements, don't want to require a server name
+                        // match for both if not needed)
+                        announcementFound = true;
+                }
+        }
 }
-
+ 
 if( !announcementFound )
 {
-	plugin.ConsoleWrite( "ERROR: No matching announcement found for next map announcement." );
-	return false;
+        plugin.ConsoleWrite( "ERROR: No matching announcement found for next map announcement." );
+        return false;
 }
-
+ 
 // OK, now print messaging, starting with any enqueued messages.
 foreach( string msg in announcementText[gameTypeSwitchingAnnouncementIndex.Value] )
 {
-	// replace the %m and %s substrings as appropriate, based on whether another round is left in this map
-	// Note that server.TotalRounds is equal to current round number MINUS ONE (i.e. counting starts at zero), so the math on the next line looks weird:
-	if( (server.TotalRounds - server.CurrentRound) > 1 )
-	{
-		replacedMsg = msg.Replace( "%m", Maps[server.MapFileName] );
-		replacedMsg = replacedMsg.Replace( "%s", " (teams switch sides)" );
-	}
-	else
-	{
-		replacedMsg = msg.Replace( "%m", Maps[server.NextMapFileName] );
-		replacedMsg = replacedMsg.Replace( "%s", "" );
-	}
-	
-	plugin.ServerCommand( "admin.say", replacedMsg, "player", player.Name );
+        // replace the %m and %s substrings as appropriate, based on whether another round is left in this map
+        // Note that server.TotalRounds is equal to current round number MINUS ONE (i.e. counting starts at zero), so the math on the next line looks weird:
+        if( (server.TotalRounds - server.CurrentRound) > 1 )
+        {
+                replacedMsg = msg.Replace( "%m", Maps[server.MapFileName] );
+                replacedMsg = replacedMsg.Replace( "%s", " (teams switch sides)" );
+        }
+        else
+        {
+                replacedMsg = msg.Replace( "%m", Maps[server.NextMapFileName] );
+                replacedMsg = replacedMsg.Replace( "%s", "" );
+        }
+ 
+        plugin.ServerCommand( "admin.say", replacedMsg, "player", player.Name );
 }
