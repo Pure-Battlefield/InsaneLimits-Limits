be8e2de061e3726c15bbb26582ee48a6533497a8
analytalica <analytalica@gmail.com>
2013-08-17 11:30:31 -0500

    added on

--- Defaults/AnnounceNextMapAndMode2.cs
+++ Defaults/AnnounceNextMapAndMode2.cs
@@ -3,7 +3,7 @@
 
 plugin.ConsoleWrite("Announcing the next map...");
 
-String map_msg = "Next map is " + plugin.FriendlyMapName(server.NextMapFileName) + " " + plugin.FriendlyModeName(server.NextGamemode);
+String map_msg = "Next map is " + plugin.FriendlyMapName(server.NextMapFileName) + " on " + plugin.FriendlyModeName(server.NextGamemode);
 String rnd_msg = "(It may be different if the server population changes enough to activate a different map rotation.) ";
 plugin.ServerCommand("admin.say", StripModifiers(E(map_msg)), "all");
 plugin.ServerCommand("admin.say", StripModifiers(E(rnd_msg)), "all");
