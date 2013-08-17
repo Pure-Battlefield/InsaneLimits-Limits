2d7fbdbfb26a8185defd5267a7886e945205ed91
analytalica <analytalica@gmail.com>
2013-08-17 13:13:00 -0500

    Final version that works

--- Defaults/AnnounceNextMapAndMode2.cs
+++ Defaults/AnnounceNextMapAndMode2.cs
@@ -1,15 +1,12 @@
-﻿if( limit.Activations() > 1 )
-        return false;
+﻿//first check code
+if((team1.RemainTicketsPercent < 75 || team2.RemainTicketsPercent < 75)) return true; return false;
 
-if((team1.RemainTicketsPercent < 15 || team2.RemainTicketsPercent < 15)){
-
+//second check code
+if(limit.Activations() > 1) return false; 
 plugin.ConsoleWrite("Announcing the next map...");
 
 String map_msg = "Next map is " + plugin.FriendlyMapName(server.NextMapFileName) + " on " + plugin.FriendlyModeName(server.NextGamemode);
 String rnd_msg = "(It may be different if the server population changes enough to activate a different map rotation.) ";
 plugin.ServerCommand("admin.say", map_msg, "all");
 plugin.ServerCommand("admin.say", rnd_msg, "all");
-
-return true;
-}
-return true;
+return true;
