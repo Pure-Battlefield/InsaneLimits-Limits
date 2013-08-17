60144af44f0db621c06f482e776c843260f0a5cf
Draegerr <tjy692@gmail.com>
2013-08-17 05:51:31 -0400

    Updated NextMapList Command

--- MainServer/NextmapRequestV2.cs
+++ MainServer/NextmapRequestV2.cs
@@ -5,8 +5,8 @@ plugin.ConsoleWrite(plugin.R("" + player.Name + " wants to know the next map"));
 
 String map_msg = "Next map is " + plugin.FriendlyMapName(server.NextMapFileName);
 String rnd_msg = "(It may be different if the server population changes enough to activate a different map rotation.) ";
-plugin.SendGlobalMessage(map_msg);
-plugin.SendGlobalMessage(rnd_msg);
+plugin.ServerCommand("admin.say", map_msg, "player", player.Name);
+plugin.ServerCommand("admin.say", rnd_msg, "player", player.Name);
 plugin.ServerCommand("admin.yell", map_msg, "8", "player", player.Name);
 
 return true;
