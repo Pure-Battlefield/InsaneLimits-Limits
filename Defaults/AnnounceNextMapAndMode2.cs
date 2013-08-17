c82e3a0f1445aa9afc0fa2b2c3861224590e3601
analytalica <analytalica@gmail.com>
2013-08-17 11:23:04 -0500

    New Anounce Next Map and Mode Plugin
    v2.0, and only 11 lines

---
+++ Defaults/AnnounceNextMapAndMode2.cs
@@ -0,0 +1,11 @@
+﻿if( limit.Activations() > 1 )
+        return false;
+
+plugin.ConsoleWrite("Announcing the next map...");
+
+String map_msg = "Next map is " + plugin.FriendlyMapName(server.NextMapFileName) + plugin.FriendlyModeName(server.NextGamemode);
+String rnd_msg = "(It may be different if the server population changes enough to activate a different map rotation.) ";
+plugin.ServerCommand("admin.say", StripModifiers(E(map_msg)), "all");
+plugin.ServerCommand("admin.say", StripModifiers(E(rnd_msg)), "all");
+
+return true;
