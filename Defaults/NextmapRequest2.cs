e49195f7f6685c68da2058bafedb0ac65525d3f3
Draegerr <tjy692@gmail.com>
2013-08-19 15:30:19 -0400

    Update NextmapRequest2.cs

--- Defaults/NextmapRequest2.cs
+++ Defaults/NextmapRequest2.cs
@@ -1,14 +1,12 @@
 //watch for !nextmap
 if (Regex.Match(player.LastChat, @"^\s*[!/](nextmap)$", RegexOptions.IgnoreCase).Success)
 {
-plugin.ConsoleWrite(plugin.R("" + player.Name + " wants to know the next map"));
+plugin.ConsoleWrite("" + player.Name + " wants to know the next map");
 
-String map_msg = "Next map is " + plugin.FriendlyMapName(server.NextMapFileName) + " on " + plugin.FriendlyModeName(server.NextGamemode);
-String rnd_msg = "The current round is " + (server.CurrentRound+1) + " of " + server.TotalRounds;
-plugin.SendGlobalMessage(map_msg);
-plugin.SendGlobalMessage(rnd_msg);
-plugin.ServerCommand("admin.yell", map_msg, "8", "player", player.Name);
-plugin.ServerCommand("admin.yell", rnd_msg, "5", "player", player.Name);
+String map_msg = "Next map is " + plugin.FriendlyMapName(server.NextMapFileName);
+String rnd_msg = "(It may be different if the server population changes enough to activate a different map rotation.) ";
+plugin.ServerCommand("admin.say", map_msg, "player", player.Name);
+plugin.ServerCommand("admin.say", rnd_msg, "player", player.Name);
 
 return true;
 }
