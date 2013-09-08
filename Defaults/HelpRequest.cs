214557bd62a94553710a918005368e9aca2b66f1
Smileynulk <smileynulk@gmail.com>
2013-09-08 00:02:01 -0700

    Update HelpRequest.cs

--- Defaults/HelpRequest.cs
+++ Defaults/HelpRequest.cs
@@ -3,7 +3,7 @@ if( Regex.Match(player.LastChat, @"^\s*[!/](help)$", RegexOptions.IgnoreCase).Su
         string help_1 = "!rules: Learn what is & isn't OK here";
         string help_2 = "!tsinfo: Squad VOIP info (TeamSpeak)";
         string help_3 = "Other commands: !aboutpure, !donate, !listadmins,";
-        string help_4 = "!nextmap, !suicide, !tshelp";
+        string help_4 = "!!nextmap, !suicide, /surrender, !tshelp";
 
         List<string> Help = new List<string>();
         Help.Add(help_1);
